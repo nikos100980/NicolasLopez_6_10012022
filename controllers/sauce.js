bb3f77ec01bd2b6310edc517ff0a819466073b5f
LOPEZ Nicolas <nikos100980@free.fr>
2022-01-23 02:03:33 +0100

    ajout du controller et de la route like

--- controllers/sauce.js
+++ controllers/sauce.js
@@ -54,6 +54,48 @@ exports.deleteSauce = (req,res,next)=>{
     .catch((error)=>res.status(404).json({ error}));
 };
 
-// exports.likeSauce = (req,res,next)=>{
-//     if()
-// };
+exports.likeSauce = (req,res,next)=>{
+    Sauce.findOne({_id:req.params.id})
+    .then((sauce)=>{
+        switch(req.body.like){
+            case 1 :
+              if(!sauce.usersLiked.includes(req.body.userId)){
+                  Sauce.updateOne({_id:req.params.id},{$inc:{likes: +1},$push:{usersLiked: req.body.userId}},
+                
+                )
+                .then(()=>res.status(201).json({message: 'Le like a bien été pris en compte'}))
+                .catch((error)=>res.status(400).json({error}));
+              } 
+              break;
+              case -1:
+                 if(!sauce.usersDisliked.includes(req.body.userId)){
+                    Sauce.updateOne({_id:req.params.id},{$inc:{dislikes: +1},$push:{usersDisliked: req.body.userId}},
+                  
+                  )
+                  .then(()=>res.status(201).json({message: 'Le dislike a bien été pris en compte'}))
+                  .catch((error)=>res.status(400).json({error}));
+                 } 
+                break;
+                case 0:
+                    if(sauce.usersLiked.includes(req.body.userId)){
+                        Sauce.updateOne({_id:req.params.id},{$inc:{likes: -1},$pull:{usersLiked: req.body.userId}},
+                
+                            )
+                            .then(()=>res.status(201).json({message: 'Le like est bien neutre'}))
+                            .catch((error)=>res.status(400).json({error}));
+                    }
+                    if(sauce.usersDisliked.includes(req.body.userId)){
+                        Sauce.updateOne({_id:req.params.id},{$inc:{dislikes: -1},$pull:{usersDisliked: req.body.userId}},
+                  
+                            )
+                            .then(()=>res.status(201).json({message: "L'utilisateur est present dans le tableau des dislike utilisateurs et le like est bien neutre"}))
+                            .catch((error)=>res.status(400).json({error}));
+                    }
+                    
+                    break;
+                    default: console.log(error);
+
+        }
+    })
+    .catch((error)=>res.status(401).json({error}))
+};
--- routes/sauce.js
+++ routes/sauce.js
@@ -10,6 +10,6 @@ router.get('/',sauceCtrl.getAllSauce);
 router.get('/:id',sauceCtrl.getOneSauce);
 router.put('/:id', sauceCtrl.modifySauce);
 router.delete('/:id',sauceCtrl.deleteSauce);
-router.post('/:id/like',)
+router.post('/:id/like',sauceCtrl.likeSauce);
 
 module.exports = router;
